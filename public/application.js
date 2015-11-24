504fc90b2eae5e4456dc799cb3f0348bdb0375ac
androidgrl <kawaharajamie@gmail.com>
2015-11-23 18:26:19 -0700

    Add socket broadcast on button click
    When the create button is clicked, then it tells
    the socket to send a message to the server.

--- public/application.js
+++ public/application.js
@@ -1,8 +1,4 @@
 const socket = io();
-var voteCounts = document.getElementById('vote-counts');
-var pollResults = document.getElementById('poll-results');
-var endPollButton = document.getElementById('end-poll');
-var votingButtons = document.querySelectorAll('input[type="radio"]');
 
 function formData(){
   return {
@@ -19,6 +15,7 @@ function postData(){
       formData(),
       function(data){
         $('#slots').append(makeSlot(data));
+        socket.send('slots', data);
         $('#start').val('');
         $('#end').val('');
         $('#date').val('');
@@ -28,51 +25,19 @@ function postData(){
 
 function makeSlot(data) {
   console.log(data);
-  var compiled = _.template("<div data-start='<%= start %>' data-end='<%= end %>' data-date='<%= date %>' data-comments='<%= comments %>'><li>Start Time: <%= start %> </li><li>End Time: <%= end %> </li><li>Date: <%= date %></li><li>Comments: <%= comments %></li></div>");
-    var newSlot = compiled({'start': data.slot.startTime,
-                            'end': data.slot.endTime,
-                            'date': data.slot.date,
-                            'comments': data.slot.comments
-                        });
-    return newSlot;
+  var compiled = _.template("<div data-start='<%= start %>' data-end='<%= end %>' data-date='<%= date %>' data-comments='<%= comments %>'><li>Start Time: <%= start %> </li><li>End Time: <%= end %> </li><li>Date: <%= date %></li><li>Comments: <%= comments %></li></div></br>");
+  var newSlot = compiled({'start': data.slot.startTime,
+    'end': data.slot.endTime,
+    'date': data.slot.date,
+    'comments': data.slot.comments
+  });
+  return newSlot;
 }
+
+socket.on('postSlots', function(data) {
+  $('#scheduling-slots').append(makeSlot(data));
+});
 
 $('document').ready(function(){
   $('#submit').on('click', postData);
 });
-
-socket.on('socketId', function(socketId) {
-  if (getCookie('socketid')) {
-    document.cookie = 'socketid=' + getCookie('socketid');
-  } else {
-    document.cookie = 'socketid=' + socketId;
-  }
-});
-
-socket.on('voteCount', function(votes) {
-  var results = '';
-
-  for (var choice in votes) {
-    results = results + '<p>' + choice + ': ' + votes[choice] + '</p>';
-  }
-
-  pollResults.innerHTML = results;
-});
-
-for (var i = 0; i < votingButtons.length; i++) {
-  votingButtons[i].addEventListener('click', function () {
-    this.dataset['socketId'] = getCookie('socketid');
-    socket.send('voteCast', this.dataset);
-  });
-}
-
-function getCookie(cname) {
-  var name = cname + '=';
-  var ca = document.cookie.split(';');
-  for(var i=0; i<ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1);
-    if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
-  }
-  return '';
-}
