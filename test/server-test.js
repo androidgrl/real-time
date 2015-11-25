cebdb3acb45f03e8742c5b5ea5e6788e79adb59d
androidgrl <kawaharajamie@gmail.com>
2015-11-24 19:59:24 -0700

    Add server tests and socket tests
    The server test needed a real schedule id
    from the redis database, which is where that long
    number in the test is coming from.
    
    The socket test is pending.

---
+++ test/server-test.js
@@ -0,0 +1,82 @@
+const assert = require('assert');
+const app = require('../index');
+const request = require('request');
+
+describe('Server', () => {
+
+  before((done) => {
+    this.port = 9876;
+
+    this.server = app.listen(this.port, (err, result) => {
+      if (err) { return done(err); }
+      done();
+    });
+
+    this.request = request.defaults({
+      baseUrl: 'http://localhost:9876/'
+    });
+  });
+
+  after(() => {
+    this.server.close();
+  });
+
+  it('should exist', () => {
+    assert(app);
+  });
+
+  describe('GET /', () => {
+    it('should return a 200', (done) => {
+      this.request.get('/', (error, response) => {
+        if (error) { done(error); }
+        assert.equal(response.statusCode, 200);
+        done();
+      });
+    });
+
+    it('should have a body with the name of the application', (done) => {
+      var title = 'Welcome to Inter-Personal Time';
+
+      this.request.get('/', (error, response) => {
+        if (error) { done(error); }
+        assert(response.body.includes(title),
+            `"${response.body}" does not include "${title}".`);
+        done();
+      });
+    });
+  });
+
+  describe('GET /admin-dashboard', () => {
+    it('should return a 200', (done) => {
+      this.request.get('/admin-dashboard', (error, response) => {
+        if (error) { done(error); }
+        assert.equal(response.statusCode, 200);
+        done();
+      });
+    });
+  });
+
+  describe('GET /admin-dashboard/3', () => {
+    it('should return a 200', (done) => {
+
+      var slot = {
+        start: "3pm",
+        end: "2pm",
+        date: "1/2/16",
+        comments: "meet with ian",
+        scheduleId: "97de4a6ac8bea261037eae55794218b612730aec"
+      };
+
+      request.post('http://localhost:9876/admin-dashboard/slots', { form: slot }, function (error, response) {
+          });
+
+      request.get('http://localhost:9876/admin-dashboard/97de4a6ac8bea261037eae55794218b612730aec', function (error, response) {
+          assert(response.body.includes("3pm"), "it should say the start");
+          assert(response.body.includes("1/2/16"), "it should say the data");
+          assert.equal(response.statusCode, 200);
+          done();
+          });
+
+    });
+  });
+});
